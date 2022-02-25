4e8e32010f456057292bc387a80341f640c43d7c
Abul Kalam Azad <dev.akazad@hotmail.com>
2022-02-25 19:49:30 +0600

    recording functionality added

--- js/app.js
+++ js/app.js
@@ -7,15 +7,48 @@ const constraint = {
 
 
 window.onload = function() {
+    const videoChunks = [];
+    let mediaRecorder;
+
+
     navigator.mediaDevices.getUserMedia(constraint).then(stream => {
 
-        const videoElement = document.querySelector('video');
+        const videoElement = document.querySelector('.video');
+        const recorderButton = document.querySelector('.recorder-btn');
+        const recorderStopButton = document.querySelector('.stop-btn');
 
 
         videoElement.srcObject = stream;
 
-        video.onloadedmetadata = function(e) {
-            video.play();
+        recorderButton.addEventListener('click', function() {
+            mediaRecorder = new MediaRecorder(stream);
+
+            mediaRecorder.start(1000);
+
+            mediaRecorder.ondataavailable = function(e) {
+                videoChunks.push(e.data);
+            }
+        });
+
+        recorderStopButton.addEventListener('click', function() {
+            mediaRecorder.stop();
+
+            const blob = new Blob(videoChunks,  {
+                type: 'video/webm'
+            });
+
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+
+            document.body.appendChild(a);
+            a.style = "display: none";
+            a.href = url;
+            a.download = 'matt_stream.webm';
+            a.click();
+        })
+
+        videoElement.onloadedmetadata = function(e) {
+            videoElement.play();
         };
     })
 }
